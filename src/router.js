d46873d798b5b1e0a0181ad6a6bd34158e2946a0
Yanny Castrillon <yannyandres117@gmail.com>
2017-05-09 16:58:01 -0700

    Merge and refactor single component, from HTML CSS and JavaScript. Use props to pass data from one component to another.

--- src/router.js
+++ src/router.js
@@ -8,24 +8,28 @@ import Home from './components/Home';
 import About from './components/About';
 import Courses from './components/Courses';
 import Teachers from './components/Teachers';
-import Course from './components/courses/Course';
-import CSS from './components/courses/CSS';
-import HTML from './components/courses/HTML';
-import JavaScript from './components/courses/JavaScript';
+import NotFound from './components/NotFound';
+
+// import Course from './components/courses/Course';
+// import CSS from './components/courses/CSS';
+import CourseContainer from './components/courses/CourseContainer';
+import CourseList from './data/courses';
+// import JavaScript from './components/courses/JavaScript';
 
 // Routes
 const routes = (
   <Router history={browserHistory}>
     <Route component={App}>
       <Route path="/" component={Home} />
-      <Route path="about" component={About} />
+      <Route path="about" component={About} title="About"/>
       <Route path="teachers" component={Teachers} />
       <Route path="courses" component={Courses}>
       <IndexRedirect to="html" />
-        <Route path="css" component={CSS}/>
-        <Route path="html" component={HTML}/>
-        <Route path="javascript" component={JavaScript}/>
+        <Route path="css" component={CourseContainer} data={CourseList.CSS}/>
+        <Route path="html" component={CourseContainer} data={CourseList.HTML}/>
+        <Route path="javascript" component={CourseContainer}data={CourseList.JS}/>
       </Route>
+      <Route path="*" component={NotFound} />
    </Route>
   </Router>
 )
